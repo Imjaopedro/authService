c2a656a254ae2f9e1c31f4a2c6cca8f2ca456042
Imjaopedro <joaovale.claudio10@gmail.com>
2025-08-14 22:43:46 -0300

    created: UserEntity class and UserRole class

---
+++ authService/src/main/java/com/authUser/service/user/RoleEnum.java
@@ -0,0 +1,19 @@
+package com.authUser.service.user;
+
+public enum RoleEnum {
+    ADMIN("admin"),
+    USER("user");
+
+    private String role;
+
+     RoleEnum (String role){
+        this.role =role;
+    }
+
+
+    public String getRole(){
+         return role;
+    }
+
+
+}
